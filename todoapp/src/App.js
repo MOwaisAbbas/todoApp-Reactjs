012eadddc8b2f180a9a77b5f0f44723865b4c140
Muhammad Owais <mowaisabbas9@gmail.com>
2023-08-11 03:11:46 +0500

    add todo functionality ADDED

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -2,8 +2,18 @@ import './App.css';
 import Footer from './components/footer/footer';
 import List from './components/todo-list/list';
 import addImg from './images/add.png'
-
+import { useState } from 'react';
 function App() {
+
+  const [inpVal, setInpVal] = useState('')
+  const [todos, setTodo] = useState([])
+const editTodo = (arr)=>{
+  console.log(todos)
+  setTodo(arr)
+  console.log(arr)
+  console.log(todos)
+}
+
   return (
     <div className="App">
       <div className='main-div'>
@@ -11,12 +21,17 @@ function App() {
           <h1>TodoApp</h1>
         </div>
         <div className='input-div'>
+          <input type='text' value={inpVal} onChange={(e) => {
+            setInpVal(e.target.value)
 
-          <input type='text' name='todo-input' id='todo-input' />
-          <button><img src={addImg} alt='add' />
+          }} name='todo-input' id='todo-input' />
+          <button onClick={() => {
+            setTodo([inpVal ,...todos])
+            setInpVal("")
+          }}><img src={addImg} alt='add' />
           </button>
         </div>
-        <List />
+        <List value={todos} again={editTodo} />
         <Footer />
       </div>
     </div>
--- todoapp/src/components/todo-list/list.js
+++ todoapp/src/components/todo-list/list.js
@@ -1,19 +1,21 @@
 import './list.css';
- import deleteImg from '../../images/delete.png'
- import editImg from '../../images/edit.png'
-const List = ()=>{
-    return<ul>
-        <li><span>Apple</span> <div> <button className='editBtn'><img src={editImg} alt='edit' /></button> <button className='dltBtn'><img src={deleteImg} alt='delete' /></button></div></li>
-        <li><span>Mango</span> <div> <button className='editBtn'><img src={editImg} alt='edit' /></button> <button className='dltBtn'><img src={deleteImg} alt='delete' /></button></div></li>
-        <li><span>Banana</span> <div> <button className='editBtn'><img src={editImg} alt='edit' /></button> <button className='dltBtn'><img src={deleteImg} alt='delete' /></button></div></li>
-        <li><span>Pineapple</span> <div> <button className='editBtn'><img src={editImg} alt='edit' /></button> <button className='dltBtn'><img src={deleteImg} alt='delete' /></button></div></li>
-        <li><span>Orange</span> <div> <button className='editBtn'><img src={editImg} alt='edit' /></button> <button className='dltBtn'><img src={deleteImg} alt='delete' /></button></div></li>
-        <li><span>Cherry</span> <div> <button className='editBtn'><img src={editImg} alt='edit' /></button> <button className='dltBtn'><img src={deleteImg} alt='delete' /></button></div></li>
-        <li><span>Papaya</span> <div> <button className='editBtn'><img src={editImg} alt='edit' /></button> <button className='dltBtn'><img src={deleteImg} alt='delete' /></button></div></li>
-        <li><span>Raspberries</span> <div> <button className='editBtn'><img src={editImg} alt='edit' /></button> <button className='dltBtn'><img src={deleteImg} alt='delete' /></button></div></li>
-        <li><span>Strawberries</span> <div> <button className='editBtn'><img src={editImg} alt='edit' /></button> <button className='dltBtn'><img src={deleteImg} alt='delete' /></button></div></li>
-        <li><span>Apricot</span> <div> <button className='editBtn'><img src={editImg} alt='edit' /></button> <button className='dltBtn'><img src={deleteImg} alt='delete' /></button></div></li>
+import deleteImg from '../../images/delete.png'
+import editImg from '../../images/edit.png' 
 
+function List({value ,again}){
+
+    
+console.log(value)
+    return <ul>{
+        value.map((v, i) =>
+
+            <li key={i}><span>{v}</span> <div> <button className='editBtn'><img src={editImg} alt='edit' /></button> <button onClick={() => {
+               value.splice(i,1)
+               again(value)
+            }
+            } className='dltBtn'><img src={deleteImg} alt='delete' /></button></div></li>
+        )
+    }
     </ul>
 }
 
